bda2a4396dd78392c4c69f87e3ae29dd3f314f34
CorentinAT <corentin.al2004@gmail.com>
2026-06-11 14:20:44 +0200

    fin modelisation gare

--- TD07/gare.cpp
+++ TD07/gare.cpp
@@ -2,127 +2,253 @@
 #include "draw_scene.hpp"
 #include "gare.hpp"
 
-void drawStation(){
+void drawPillar(float x, float y) {
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.setFlatColor(1.0, 0.8, 0.7);
+	myEngine.mvMatrixStack.addTranslation({x, y, 0.f});
+	myEngine.mvMatrixStack.addHomothety({1.f, 1.f, 8.f});
+	myEngine.updateMvMatrix();
+	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
+}
+
+void drawRoofPart(float x, float rotation) {
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.setFlatColor(0.4, 0.4, 0.4);
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation({x, 0.f, 0.f});
+	myEngine.updateMvMatrix();
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addRotation(rotation, {0.f, 1.f, 0.f});
+	myEngine.updateMvMatrix();
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addHomothety({0.5f, 20.f, 4.7f});
+	myEngine.updateMvMatrix();
+	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
+	myEngine.mvMatrixStack.popMatrix();
+	myEngine.mvMatrixStack.popMatrix();
+	myEngine.mvMatrixStack.popMatrix();
+}
+
+void drawRoof() {
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation({0.f, 0.f, 5.5f});
+	drawRoofPart(-1.5f, M_PI / 4.f);
+	drawRoofPart(1.5f, -M_PI / 4.f);
+	myEngine.mvMatrixStack.popMatrix();
+}
+
+void drawRoofClosures() {
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.setFlatColor(0.4, 0.4, 0.4);
+	myEngine.mvMatrixStack.addTranslation({0.f, 0.f, 4.f});
+	myEngine.mvMatrixStack.addRotation(M_PI / 4.f, {0.f, 1.f, 0.f});
+	myEngine.updateMvMatrix();
 	
-	myEngine.mvMatrixStack.pushMatrix(); // all
-	myEngine.setFlatColor(1.0,0.0,0.0);
+	for(int i = 0; i < 2; i++) {
+		myEngine.mvMatrixStack.pushMatrix();
+		myEngine.mvMatrixStack.addTranslation({0.f, (8.4f * (1 - 2*i)), 0.f});
+		myEngine.mvMatrixStack.addHomothety({4.5f, 1.f, 4.5f});
+		myEngine.updateMvMatrix();
+		cube->draw();
+		myEngine.mvMatrixStack.popMatrix();
+	}
+	myEngine.mvMatrixStack.popMatrix();
+}
 
-	myEngine.mvMatrixStack.addTranslation({10.f*stationPlacement[0],10.f*stationPlacement[1],0.f});
-	myEngine.mvMatrixStack.addTranslation({5.f,10.f,3.f});
+void drawLiaisionRoofPart(float y, float rotation) {
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.setFlatColor(0.4, 0.4, 0.4);
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation({0.f, y, 0.f});
 	myEngine.updateMvMatrix();
-
-	myEngine.mvMatrixStack.pushMatrix(); // centre
-
-	myEngine.mvMatrixStack.addHomothety({6.f,18.f,8.f});
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addRotation(rotation, {1.f, 0.f, 0.f});
 	myEngine.updateMvMatrix();
-
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addHomothety({16.f, 0.5f, 3.8f});
+	myEngine.updateMvMatrix();
 	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
+	myEngine.mvMatrixStack.popMatrix();
+	myEngine.mvMatrixStack.popMatrix();
+	myEngine.mvMatrixStack.popMatrix();
+}
 
-	myEngine.mvMatrixStack.popMatrix(); // centre
-	myEngine.mvMatrixStack.pushMatrix(); // Pilier 1
-
-	myEngine.setFlatColor(1.0,0.8,0.7);
-
-	myEngine.mvMatrixStack.addTranslation({-3.f,-9.f,0.f});
-	myEngine.mvMatrixStack.addHomothety({1.f,1.f,8.f});
+void drawLiaisionRoof() {
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation({9.f, 0.f, 5.f});
 	myEngine.updateMvMatrix();
-
-	cube->draw();
-
-	myEngine.mvMatrixStack.popMatrix(); // Pilier 1
-	myEngine.mvMatrixStack.pushMatrix(); // Pilier 2
-
-	myEngine.mvMatrixStack.addTranslation({3.f,-9.f,0.f});
-	myEngine.mvMatrixStack.addHomothety({1.f,1.f,8.f});
+	
+	drawLiaisionRoofPart(-1.5f, -M_PI / 3.f);
+	drawLiaisionRoofPart(1.5f, M_PI / 3.f);
+	
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation({0.f, 0.f, -1.f});
+	myEngine.mvMatrixStack.addHomothety({16.f, 6.f, 0.3f});
 	myEngine.updateMvMatrix();
-
 	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
+	
+	myEngine.mvMatrixStack.popMatrix();
+}
 
-	myEngine.mvMatrixStack.popMatrix(); // Pilier 2
-	myEngine.mvMatrixStack.pushMatrix(); // Pilier 3
-
-	myEngine.mvMatrixStack.addTranslation({3.f,9.f,0.f});
-	myEngine.mvMatrixStack.addHomothety({1.f,1.f,8.f});
+void drawClock() {
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.setFlatColor(1.0, 1.0, 0.0);
+	myEngine.mvMatrixStack.addTranslation({1.6f, 0.f, 1.f});
+	myEngine.mvMatrixStack.addRotation(M_PI / 2.f, {0.f, 0.f, 1.f});
+	
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation({0.f, 0.1f, 0.f});
+	myEngine.mvMatrixStack.addHomothety({1.f, 0.1f, 1.f});
 	myEngine.updateMvMatrix();
+	sphere->draw();
+	myEngine.mvMatrixStack.popMatrix();
+	
+	myEngine.setFlatColor(0.0, 0.0, 0.0);
+	
+	for(int i = 0; i < 2; i++) {
+		myEngine.mvMatrixStack.pushMatrix();
+		float angle = (i == 0) ? (M_PI / 4.f) : (-M_PI / 3.f);
+		float z = (i == 0) ? (0.45f) : (0.25f);
+		float scale = (i == 0) ? (0.9f) : (0.5f);
+		myEngine.mvMatrixStack.addRotation(angle, {0.f, 1.f, 0.f});
+		myEngine.mvMatrixStack.addTranslation({0.f, 0.f, z});
+		myEngine.mvMatrixStack.addHomothety({0.1f, 0.1f, scale});
+		myEngine.updateMvMatrix();
+		cube->draw();
+		myEngine.mvMatrixStack.popMatrix();
+	}
+	
+	myEngine.mvMatrixStack.popMatrix();
+}
 
-	cube->draw();
-
-	myEngine.mvMatrixStack.popMatrix(); // Pilier 3
-	myEngine.mvMatrixStack.pushMatrix(); // Pilier 4
-
-	myEngine.mvMatrixStack.addTranslation({-3.f,9.f,0.f});
-	myEngine.mvMatrixStack.addHomothety({1.f,1.f,8.f});
+void drawStone(float angle, float radius, bool noRotation) {
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation({0.f, radius * std::cos(angle), 1.25f + radius * std::sin(angle)});
+	if(!noRotation) {
+		myEngine.mvMatrixStack.addRotation(angle + M_PI / 2.f, {1.f, 0.f, 0.f});
+	} else {
+		myEngine.mvMatrixStack.addRotation(M_PI / 2.f, {1.f, 0.f, 0.f});
+	}
+	myEngine.mvMatrixStack.addHomothety({0.4f, 0.8f, 0.4f});
 	myEngine.updateMvMatrix();
-
 	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
+}
 
-	myEngine.mvMatrixStack.popMatrix(); // Pilier 4
-	myEngine.mvMatrixStack.pushMatrix(); // Toit
+void drawWindowStones() {
+	myEngine.setFlatColor(0.4, 0.4, 0.4);
+	for(int i = 0; i < 7; i++) {
+		float angle = (M_PI / 5.f) * i - M_PI / 10.f;
+		float radius = 1.5f;
+		drawStone(angle, radius, i==0 || i==6);
+	}
+}
 
-	myEngine.setFlatColor(0.4,0.4,0.4);
+void drawWindow(float y) {
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation({0.f, y, 0.f});
+	myEngine.updateMvMatrix();
+	
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.setFlatColor(0.4, 0.4, 1.0);
+	myEngine.mvMatrixStack.addHomothety({0.1f, 2.5f, 2.5f});
+	myEngine.updateMvMatrix();
+	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
+	
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation({0.f, 0.f, 1.25f});
+	myEngine.mvMatrixStack.addHomothety({0.1f, 1.25f, 1.25f});
+	myEngine.updateMvMatrix();
+	sphere->draw();
+	myEngine.mvMatrixStack.popMatrix();
+	
+	drawWindowStones();
+	
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.setFlatColor(0.4, 0.4, 0.4);
+	myEngine.mvMatrixStack.addTranslation({0.f, 0.f, -1.25f});
+	myEngine.mvMatrixStack.addHomothety({1.f, 2.5f, 0.5f});
+	myEngine.updateMvMatrix();
+	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
+	
+	myEngine.mvMatrixStack.popMatrix();
+}
+
+void drawFacade() {
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation({3.f, 0.f, 0.f});
+	myEngine.updateMvMatrix();
+	
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.setFlatColor(0.4, 0.4, 1.0);
+	myEngine.mvMatrixStack.addTranslation({0.f, 0.f, -1.f});
+	myEngine.mvMatrixStack.addHomothety({0.1f, 3.5f, 5.f});
+	myEngine.updateMvMatrix();
+	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
+	
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.mvMatrixStack.addTranslation({0.f, 0.f, 1.5f});
+	myEngine.mvMatrixStack.addHomothety({0.1f, 1.75f, 1.75f});
+	myEngine.updateMvMatrix();
+	sphere->draw();
+	myEngine.mvMatrixStack.popMatrix();
+	
+	drawWindow(5.5f);
+	drawWindow(-5.5f);
+	
+	myEngine.mvMatrixStack.popMatrix();
+}
+
+void drawStation(){
+	myEngine.mvMatrixStack.pushMatrix();
+	myEngine.setFlatColor(1.0, 0.0, 0.0);
+
+	myEngine.mvMatrixStack.addTranslation({10.f*stationPlacement[0], 10.f*stationPlacement[1], 0.f});
+	myEngine.mvMatrixStack.addTranslation({5.f, 10.f, 3.f});
+	myEngine.updateMvMatrix();
 
 	myEngine.mvMatrixStack.pushMatrix();
-	myEngine.mvMatrixStack.addTranslation({-1.5f,0.f,5.5f});
+	myEngine.mvMatrixStack.addHomothety({6.f, 18.f, 8.f});
 	myEngine.updateMvMatrix();
-
-	myEngine.mvMatrixStack.pushMatrix();
-	myEngine.mvMatrixStack.addRotation(M_PI/4.f, {0.f,1.f,0.f});
-	myEngine.updateMvMatrix();
-
-	myEngine.mvMatrixStack.pushMatrix();
-
-	myEngine.mvMatrixStack.addHomothety({0.5f,20.f,4.7f});
-	myEngine.updateMvMatrix();
-
 	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
 
-	myEngine.mvMatrixStack.popMatrix(); // Toit
-	myEngine.mvMatrixStack.popMatrix(); // Toit
-	myEngine.mvMatrixStack.popMatrix(); // Toit
-	myEngine.mvMatrixStack.popMatrix(); // Toit
+	drawPillar(-3.f, -9.f);
+	drawPillar(3.f, -9.f);
+	drawPillar(3.f, 9.f);
+	drawPillar(-3.f, 9.f);
 
-	myEngine.mvMatrixStack.pushMatrix(); // Toit
+	drawRoof();
+	drawRoofClosures();
+
+	drawPillar(16.f, 2.5f);
+	drawPillar(16.f, -2.5f);
+
+	drawLiaisionRoof();
 
 	myEngine.mvMatrixStack.pushMatrix();
-	myEngine.mvMatrixStack.addTranslation({1.5f,0.f,5.5f});
+	myEngine.setFlatColor(0., 1., 0.);
+	myEngine.mvMatrixStack.addTranslation({0.f, 0.f, 7.f});
 	myEngine.updateMvMatrix();
 
 	myEngine.mvMatrixStack.pushMatrix();
-	myEngine.mvMatrixStack.addRotation(-M_PI/4.f, {0.f,1.f,0.f});
+	myEngine.mvMatrixStack.addHomothety({3.f, 3.f, 5.f});
 	myEngine.updateMvMatrix();
-
-	myEngine.mvMatrixStack.pushMatrix();
-
-	myEngine.mvMatrixStack.addHomothety({0.5f,20.f,4.7f});
-	myEngine.updateMvMatrix();
-
 	cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
 
-	myEngine.mvMatrixStack.popMatrix(); // Toit
-	myEngine.mvMatrixStack.popMatrix(); // Toit
-	myEngine.mvMatrixStack.popMatrix(); // Toit
-	myEngine.mvMatrixStack.popMatrix(); // Toit
+	drawClock();
 
-	myEngine.mvMatrixStack.pushMatrix(); // Pilier 5
+	myEngine.mvMatrixStack.popMatrix();
 
-	myEngine.setFlatColor(1.0,0.8,0.7);
+	drawFacade();
 
-	myEngine.mvMatrixStack.addTranslation({16.f,3.f,0.f});
-	myEngine.mvMatrixStack.addHomothety({1.f,1.f,8.f});
-	myEngine.updateMvMatrix();
-
-	cube->draw();
-
-	myEngine.mvMatrixStack.popMatrix(); // Pilier 5
-	myEngine.mvMatrixStack.pushMatrix(); // Pilier 6
-
-	myEngine.mvMatrixStack.addTranslation({16.f,-3.f,0.f});
-	myEngine.mvMatrixStack.addHomothety({1.f,1.f,8.f});
-	myEngine.updateMvMatrix();
-
-	cube->draw();
-
-	myEngine.mvMatrixStack.popMatrix(); // Pilier 6
-
-	myEngine.mvMatrixStack.popMatrix(); // all
+	myEngine.mvMatrixStack.popMatrix();
 }
